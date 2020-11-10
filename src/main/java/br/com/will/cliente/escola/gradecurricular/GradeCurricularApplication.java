7d98c7533fb717d7d1ad34aa51c9e9b80057c4d4
williamisfranciscodasilva <williamisepaula@gmail.com>
2020-11-10 04:07:18 -0300

    criação do projeto

---
+++ src/main/java/br/com/will/cliente/escola/gradecurricular/GradeCurricularApplication.java
@@ -0,0 +1,13 @@
+package br.com.will.cliente.escola.gradecurricular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeCurricularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeCurricularApplication.class, args);
+	}
+
+}
